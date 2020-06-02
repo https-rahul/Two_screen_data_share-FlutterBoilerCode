00d67597f0091d476b17afa295dc8cb2542e39e7
Rahul Gaur <rgaur1620@gmail.com>
2020-06-02 14:49:18 +0530

    added all

---
+++ android/app/src/main/java/com/rahul/twoscreenboilerplate/MainActivity.java
@@ -0,0 +1,6 @@
+package com.rahul.twoscreenboilerplate;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+void main() {
+  runApp(new MaterialApp(
+    title: 'Screens',
+    home: new Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _nameFieldController = new TextEditingController();
+
+
+
+  Future _goToNextScreen(BuildContext context) async {
+    Map results = await Navigator.of(context)
+        .push(new MaterialPageRoute(builder: (BuildContext context) {
+      return NextScreen(name: _nameFieldController.text);
+    }));
+
+    if (results != null && results.containsKey("info")) {
+      print(results['info'].toString());
+      _nameFieldController.text = results['info'].toString();
+    }
+    else {
+      print("Noting");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('First Screen'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: new ListView(
+        children: <Widget>[
+          new ListTile(
+            title: new TextField(
+              controller: _nameFieldController,
+              decoration: new InputDecoration(labelText: 'Your name'),
+            ),
+          ),
+          new ListTile(
+            title: new RaisedButton(
+                child: new Text("Next"),
+                onPressed: () {
+                  _goToNextScreen(context);
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NextScreen extends StatefulWidget {
+  final String name;
+
+  const NextScreen({Key key, this.name}) : super(key: key);
+
+  @override
+  _NextScreenState createState() => _NextScreenState();
+}
+
+class _NextScreenState extends State<NextScreen> {
+  var _backTextFieldController = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.greenAccent,
+        title: new Text('Second Screen'),
+        centerTitle: true,
+      ),
+      body: new Container(
+        child: new Column(
+          children: <Widget>[
+            new ListTile(
+              title: new Text('${widget.name}'),
+            ),
+            new ListTile(
+              title: new TextField(
+                controller: _backTextFieldController,
+              ),
+            ),
+            new ListTile(
+              title: new FlatButton(
+                  onPressed: () {
+                    Navigator.pop(
+                        context, {'info': _backTextFieldController.text});
+                  },
+                  child: new Text('Send back')),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
